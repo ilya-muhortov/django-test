1c755e56f65b2d75fdf76b02c805e7ed7f457bc6
Ilya Muhortov <ilya.muhortov@gmail.com>
2014-01-08 11:32:56 +1100

    fixes

--- project/settings.py
+++ project/settings.py
@@ -86,15 +86,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "project/static")
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "project/static"),
-)
+STATICFILES_DIRS = ()
 
 DATE_INPUT_FORMATS = ('%d.%m.%Y',)
 DATE_FORMAT = 'd.m.Y'
 
-FORMAT_MODULE_PATH = 'project.formats'
-
-import locale
-locale.setlocale(locale.LC_TIME, 'ru_RU')
+try:
+    from project.settings_private import *
+except ImportError:
+    pass
